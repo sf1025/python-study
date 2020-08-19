372cf7fa95f5daa5bfc7f2b6aecfb9028dc1fac5
unknown <596447006@qq.com>
2020-08-19 14:59:56 +0800

    py定义方法-pass用法

---
+++ test_demo/def-func.py
@@ -0,0 +1,10 @@
+#coding:utf-8
+#!usr/bin/env python
+#pass是空语句, 为了保持程序结构的完整性
+#pass不做任何事情,一般用作占位语句
+def func():
+    #空函数在python2.x版本中pass是必须的
+    pass
+def func1():
+    #在python3.x中pass可以写或者不写
+    pass
